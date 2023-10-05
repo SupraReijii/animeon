e843f6cecee27f9c454ef7ff742756e3fc1e3dd6
Daniil Markov <reijii.jp@icloud.com>
2023-10-05 13:49:24 +0300

    fix styles

--- config/webpack/environment.js
+++ config/webpack/environment.js
@@ -1,4 +1,6 @@
+const webpack = require('webpack');
 const { environment } = require('@rails/webpacker')
+const path = require('path');
 
 const coffeeLoader = require('./loaders/coffee');
 environment.loaders.append('coffee', coffeeLoader);
