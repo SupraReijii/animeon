c5e158f74c880b5ca68b204139a738b9c448655c
Daniil Markov <reijii.jp@icloud.com>
2023-10-05 13:41:03 +0300

    fix styles

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -64,6 +64,7 @@ Rails.application.configure do
 
   config.action_mailer.perform_caching = false
   config.hosts << 'animeon.ru'
+  config.hosts << 'animeon.local'
   # Ignore bad email addresses and do not raise email delivery errors.
   # Set this to true and configure the email server for immediate delivery to raise delivery errors.
   # config.action_mailer.raise_delivery_errors = false
