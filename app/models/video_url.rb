5b41efd96954f101ca0f1f22ef74c94802c0b9d9
SupraReijii <reijii.jp@icloud.com>
2025-03-02 21:14:58 +0300

    fix url

--- app/models/video_url.rb
+++ app/models/video_url.rb
@@ -8,19 +8,19 @@ class VideoUrl < ApplicationRecord
   before_save :add_priority
   before_save :add_url
   def add_url
-    self[:url] = "https://storage.animeon.ru/#{self[:video_id]}/#{self[:quality].match('\d+')[0]}.m3u8"
+    self[:url] = "https://storage.animeon.ru/video/#{self[:video_id]}/#{self[:quality].match('\d+')[0]}.m3u8"
   end
 
   def add_priority
     q = {
-      "1440p" => 0,
-      "1080p" => 1,
-      "720p" => 2,
-      "480p" => 3,
-      "360p" => 4,
-      "240p" => 5,
-      "144p" => 6,
-      "unknown" => 7
+      '1440p' => 0,
+      '1080p' => 1,
+      '720p' => 2,
+      '480p' => 3,
+      '360p' => 4,
+      '240p' => 5,
+      '144p' => 6,
+      'unknown' => 7
     }
     self[:priority] = q[(self[:quality]).to_s]
   end
