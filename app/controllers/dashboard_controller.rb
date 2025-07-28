cf95fb0fd98c0abe52b877bbfe0ed548beef02ee
Daniil Markov <reijii.jp@icloud.com>
2025-07-28 16:20:03 +0300

    dashboard improve

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,2 +1,6 @@
 class DashboardController < ApplicationController
+  def index
+    @animes = Anime.where(status: :ongoing).limit(10).order(user_rating: :desc).shuffle
+    @animes = @animes[0..4]
+  end
 end
