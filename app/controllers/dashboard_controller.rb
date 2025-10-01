f9b08e24b05b5b883e5c248a4513ff239a05b77b
Daniil Markov <reijii.jp@icloud.com>
2025-10-01 08:58:49 +0300

    some og changes

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -1,6 +1,6 @@
 class DashboardController < ApplicationController
   def index
-    @animes = Anime.where(status: :ongoing, kind: :tv).where("user_rating > ?", 7).limit(10).order(user_rating: :desc).shuffle
+    @animes = Anime.where(status: :ongoing, kind: :tv).where('user_rating > ?', 6).limit(10).order(user_rating: :desc).shuffle
     @animes = @animes[0..4]
   end
 end
