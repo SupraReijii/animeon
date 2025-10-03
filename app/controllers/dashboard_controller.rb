026aecc46831e973102e468b71d7a02c86370ebb
SupraReijii <reijii.jp@icloud.com>
2025-10-03 11:24:05 +0300

    add title to root page

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -2,5 +2,6 @@ class DashboardController < ApplicationController
   def index
     @animes = Anime.where(status: :ongoing, kind: :tv).where("user_rating > ?", 7).limit(10).order(user_rating: :desc).shuffle[0..4]
     @animes_with_video = Anime.joins(episode: :video).distinct.limit(10).shuffle[0..4]
+    @title = 'Главная страница'
   end
 end
