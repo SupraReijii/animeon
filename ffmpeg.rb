ceea23704e3dff54cf1a2e58ea806937c5054ee0
SupraReijii <reijii.jp@icloud.com>
2025-10-12 21:42:38 +0300

    Improved search block and transcoder

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -2,26 +2,35 @@
 
 require 'pg'
 require 'streamio-ffmpeg'
+require 'redis'
 
 i = 0
-conn = PG::Connection.new(host: ENV['DATABASE_HOST'], user: 'animeon_dev', password: ENV['DATABASE_PASSWORD'])
+conn = PG::Connection.new(host: ENV['DATABASE_HOST'], user: 'animeon_prod', password: ENV['DATABASE_PASSWORD'])
+redis = Redis.new(host: '45.84.1.34', password: ENV['REDIS_PASSWORD'])
+redis.set("transcoder:status", "active")
+redis.set("transcoder:iterations", "0")
+redis.set("transcoder:videos", "0")
+redis.set("transcoder:stop", "0")
 while i != -1
   res = conn.exec('SELECT * FROM videos WHERE status = 0 ORDER BY id LIMIT 1').first
   puts res
   unless res.nil?
     id = res['id']
-    format = res['video_file_file_name'].match('(.mp4|.avi|.mkv|.mov|.ts)')
+    format = res['video_file_file_name'].match('(\.mp4|\.avi|\.mkv|\.mov|\.ts)')
     movie = FFMPEG::Movie.new("/mnt/video/#{id}/video-#{id}#{format}")
     if movie.valid?
       res['quality'][1, res['quality'].size - 2].split(',').each do |q|
         Dir.mkdir("/mnt/video/#{id}/#{q.gsub('p', '')}")
       end
       conn.exec("UPDATE videos SET status = 1 WHERE id = #{id}")
+      redis.set("transcoder:status", "transcoding")
       system("sh transcode -i #{id} -f #{format}")
     end
     conn.exec("UPDATE videos SET status = 2 WHERE id = #{id}")
+    redis.incr("transcoder:videos")
   end
   sleep(5)
-  File.read('control').to_s.strip == 'stop' ? i = -1 : i += 1
-  puts i
+  redis.get("transcoder:stop") == 1 ? i = -1 : i += 1
+  redis.incr("transcoder:iterations")
 end
+redis.set("transcoder:status", "stopped")
