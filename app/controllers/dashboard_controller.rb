dc8cd8f3ebc16e2f37b3e54b651ec9e95ac8559c
SupraReijii <reijii.jp@icloud.com>
2025-10-16 22:39:03 +0300

    fix bug

--- app/controllers/dashboard_controller.rb
+++ app/controllers/dashboard_controller.rb
@@ -3,6 +3,6 @@ class DashboardController < ApplicationController
     @animes = Anime.where(status: :ongoing, kind: :tv).where("user_rating > ?", 7).limit(10).order(user_rating: :desc).shuffle[0..4]
     @animes_with_video = Anime.joins(episode: :video).distinct.limit(10).shuffle[0..4]
     @title = 'Главная страница'
-    @news = News.where(is_public: true).order(public_after: :desc).limit(5)
+    @news = News.where(is_public: true).order(public_after: :asc).limit(5)
   end
 end
