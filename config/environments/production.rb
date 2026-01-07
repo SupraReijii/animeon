6f3580c03520ccfafd407001e8f4c764f1b0cec4
SupraReijii <reijii.jp@icloud.com>
2026-01-07 12:43:11 +0300

    testing mails

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -23,4 +23,16 @@ Rails.application.configure do
   config.i18n.fallbacks = true
   config.active_support.report_deprecations = false
   config.active_record.dump_schema_after_migration = false
+  config.action_mailer.default_url_options = {:host => 'animeon.ru'}
+  config.action_mailer.default_options = { from: "no-reply@animeon.ru" }
+  config.action_mailer.delivery_method = :smtp
+  config.action_mailer.smtp_settings = {
+    address: "smtp.yandex.ru",
+    port: 465,
+    domain: 'animeon.ru',
+    user_name: Rails.application.credentials.dig(:smtp, :user_name),
+    password: Rails.application.credentials.dig(:smtp, :password),
+    authentication:  "plain",
+    enable_starttls: true
+  }
 end
