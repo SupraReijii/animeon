f29e8534bdc0d764e28374206d7d6fb249019dd8
SupraReijii <reijii.jp@icloud.com>
2026-01-27 11:19:21 +0300

    refactor video transcoding

--- ffmpeg.rb
+++ ffmpeg.rb
@@ -2,9 +2,14 @@
 
 require 'pg'
 require 'streamio-ffmpeg'
-require 'redis'
+require 'REDIS'
 require 'aws-sdk-s3'
 require 'dotenv'
+require 'logger'
+
+file = File.open('transcoder.log', File::WRONLY | File::APPEND | File::CREAT)
+LOGGER = Logger.new(file, 'weekly')
+LOGGER.level = Logger::DEBUG
 
 Dotenv.load('/home/devops/animeon/.env')
 Aws.config.update(
@@ -14,57 +19,73 @@ Aws.config.update(
   force_path_style: true,
   signature_version: 'v4'
 )
-aws_client = Aws::S3::Client.new
+CLIENT = Aws::S3::Client.new
 Aws.config.update(logger: Logger.new($stdout), log_level: :debug)
 
-i = 0
-conn = PG::Connection.new(host: ENV['DATABASE_HOST'], user: 'animeon', password: ENV['DATABASE_PASSWORD'], port: 54320, dbname: 'animeon_prod')
-redis = Redis.new(host: '45.84.1.34', port: ENV['REDIS_PORT'])
-redis.set("transcoder:status", "active")
-redis.set("transcoder:iterations", "0")
-redis.set("transcoder:videos", "0")
-redis.set("transcoder:stop", "0")
-redis.set("transcoder:current", "0")
-redis.set("transcoder:current_time_start", "0")
-while i != -1
-  res = conn.exec('SELECT * FROM videos WHERE status = 0 ORDER BY id LIMIT 1').first
-  puts res
-  unless res.nil?
-    id = res['id']
-    redis.set("transcoder:current", "#{id}")
-    format = res['video_file_file_name'].match('(\.mp4|\.avi|\.mkv|\.mov|\.ts)')
-    system("sudo -u devops mkdir /transcoding/#{id}")
-    aws_client.get_object(
-      bucket: 'video',
-      key: "#{id}/video-#{id}#{format}",
-      response_target: "/transcoding/#{id}/video-#{id}#{format}"
-    )
-    movie = FFMPEG::Movie.new("/transcoding/#{id}/video-#{id}#{format}")
-    if movie.valid?
-      conn.exec("UPDATE videos SET status = 1 WHERE id = #{id}")
-      redis.set("transcoder:status", "transcoding")
-      redis.set("transcoder:current_time_start", Time.now.to_s)
-      time_start = Time.now
-      system("sh /home/devops/transcode -i #{id} -f #{format.to_s.gsub('.', '')}")
-      time_end = (Time.now - time_start).round(0)
-      system("aws s3 cp --recursive /transcoding/#{id} s3://video/#{id}")
-      redis.set("transcoder:video:#{redis.get("transcoder:videos_all_time")}:time",
-                time_end.to_s)
-      redis.set("transcoder:video:#{redis.get("transcoder:videos_all_time")}:id",
-                id.to_s)
-      redis.set("transcoder:videos_all_time_transcoding_time",
-                "#{(redis.get("transcoder:videos_all_time_transcoding_time").to_i + time_end)}")
+LOGGER.info('initialize') { "Initializing..." }
+CONN = PG::Connection.new(host: ENV['DATABASE_HOST'], user: 'animeon', password: ENV['DATABASE_PASSWORD'], port: 54320, dbname: 'animeon_prod')
+REDIS = Redis.new(host: '45.84.1.34', port: ENV['REDIS_PORT'])
+
+REDIS.set("transcoder:status", "active")
+REDIS.set("transcoder:iterations", "0")
+REDIS.set("transcoder:videos", "0")
+REDIS.set("transcoder:stop", "0")
+REDIS.set("transcoder:current", "0")
+REDIS.set("transcoder:current_time_start", "0")
+def main
+  i = 0
+  while i != -1
+    LOGGER.debug("iteration - #{REDIS.get("transcoder:iterations")}")
+    res = CONN.exec('SELECT * FROM videos WHERE status = 0 ORDER BY id LIMIT 1').first
+    
+    unless res.nil?
+      id = res['id']
+      LOGGER.debug("Find video with id = #{id}")
+      REDIS.set("transcoder:current", "#{id}")
+      format = res['video_file_file_name'].match('(\.mp4|\.avi|\.mkv|\.mov|\.ts)')
+      system("sudo -u devops mkdir /transcoding/#{id}")
+      LOGGER.debug("Starting to downloading video with id = #{id}")
+      CLIENT.get_object(
+        bucket: 'video',
+        key: "#{id}/video-#{id}#{format}",
+        response_target: "/transcoding/#{id}/video-#{id}#{format}"
+      )
+      movie = FFMPEG::Movie.new("/transcoding/#{id}/video-#{id}#{format}")
+      if movie.valid?
+        LOGGER.debug("Starting to transcoding video with id = #{id}")
+        CONN.exec("UPDATE videos SET status = 1 WHERE id = #{id}")
+        REDIS.set("transcoder:status", "transcoding")
+        REDIS.set("transcoder:current_time_start", Time.now.to_s)
+        time_start = Time.now
+        system("sh /home/devops/transcode -i #{id} -f #{format.to_s.gsub('.', '')}")
+        time_end = (Time.now - time_start).round(0)
+        LOGGER.debug("Successfully transcoded video with id = #{id}")
+        LOGGER.debug("Starting to uploading video with id = #{id}")
+        system("aws s3 cp --recursive /transcoding/#{id} s3://video/#{id}")
+        LOGGER.debug("Successfully uploaded video with id = #{id}")
+        REDIS.set("transcoder:video:#{REDIS.get("transcoder:videos_all_time")}:time",
+                  time_end.to_s)
+        REDIS.set("transcoder:video:#{REDIS.get("transcoder:videos_all_time")}:id",
+                  id.to_s)
+        REDIS.set("transcoder:videos_all_time_transcoding_time",
+                  "#{(REDIS.get("transcoder:videos_all_time_transcoding_time").to_i + time_end)}")
+      end
+      CONN.exec("UPDATE videos SET status = 2 WHERE id = #{id}")
+      REDIS.incr("transcoder:videos")
+      REDIS.incr("transcoder:videos_all_time")
+      REDIS.set("transcoder:current_time_start", "0")
+      REDIS.set("transcoder:current", "0")
+      REDIS.set("transcoder:status", "active")
+      #system("rm -r /transcoding/#{id}")
     end
-    conn.exec("UPDATE videos SET status = 2 WHERE id = #{id}")
-    redis.incr("transcoder:videos")
-    redis.incr("transcoder:videos_all_time")
-    redis.set("transcoder:current_time_start", "0")
-    redis.set("transcoder:current", "0")
-    redis.set("transcoder:status", "active")
-    #system("rm -r /transcoding/#{id}")
+    sleep(5)
+    REDIS.get("transcoder:stop") == "1" ? i = -1 : i += 1
+    REDIS.incr("transcoder:iterations")
   end
-  sleep(5)
-  redis.get("transcoder:stop") == "1" ? i = -1 : i += 1
-  redis.incr("transcoder:iterations")
+rescue => err
+  REDIS.set("transcoder:status", "error")
+  LOGGER.fatal("Caught exception; exiting")
+  LOGGER.fatal(err)
 end
-redis.set("transcoder:status", "stopped")
+
+REDIS.set("transcoder:status", "stopped")
